5ee9cb791024e10e1f3d2d08497eb39d2f5df011
esteban19 <EstebanMCG@gmail.com>
2014-01-20 18:41:28 -0500

    Version 1.0.0.0

---
+++ PrintMultiThreading.Tests/PrintMultiThreadingTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrintMultiThreading;
+
+namespace PrintMultiThreading.Tests
+{
+    [TestClass]
+    public class PrintMultiThreadingTests
+    {
+        [TestMethod]
+        public void Sum_AddingASequenceOfNumbers_ShouldReturnSummationOfThem()
+        {
+            Printer p = new Printer();
+            int expected = 11;
+            int result = p.Sum(10, 1);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ PrintMultiThreading/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PrintMultiThreading
+{
+    public class Printer
+    {
+        private object threadLock = new object();
+
+        public int Sum(int toAdd, int index)
+        {
+            return toAdd + index;
+        }
+
+        public void PrintNumbers()
+        {
+            lock (threadLock)
+            {
+                Console.WriteLine("-> {0} is executing PrintNumbers()", Thread.CurrentThread.Name);
+
+                Console.Write("Numbers: ");
+                int summation = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    Random r = new Random();
+                    Thread.Sleep(1000 * r.Next(5));
+                    summation = Sum(summation, i);
+                    Console.Write("{0}, ", summation);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer p = new Printer();
+
+            Thread[] threads = new Thread[10];
+            for (int i = 0; i < 10; i++)
+            {
+                threads[i] = new Thread(new ThreadStart(p.PrintNumbers));
+                threads[i].Name = string.Format("Worker thread #{0}", i);
+            }
+
+            foreach (Thread t in threads)
+            {
+                t.Start();
+            }
+        }
+    }
+}
